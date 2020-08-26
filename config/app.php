7d7c1d34f20d7ab2f691b058f188103ca44f2d15
heyu <heyu@ketangpai.com>
2020-08-26 11:45:48 +0800

    websocket连接地址加入配置项

--- config/app.php
+++ config/app.php
@@ -20,4 +20,8 @@ return [// 应用地址
 
 	// 错误显示信息,非调试模式有效
 	'error_message'    => '页面错误！请稍后再试～', // 显示错误信息
-	'show_error_msg'   => FALSE, 'auto_multi_app' => TRUE, 'global_auth_key' => 'easy-_-chat',];
+	'show_error_msg'   => FALSE,
+	'auto_multi_app'   => TRUE,
+	'global_auth_key'  => 'easy!chat!',
+	'websocket_url'    => env('app.websocket_url'),
+];
